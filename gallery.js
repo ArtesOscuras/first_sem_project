6cdda6fe9d69062f215a9145a3e1e3aee49cdc66
ArtesOscuras <artesoscuras13@gmail.com>
2016-10-18 21:09:26 +0800

    2.66
    Changed how modals worked a little.
    More pictures
    Added modal image navigation
    Changed Footer
    
    Form not yet fixed. Should be fixed by 2.7

--- gallery.js
+++ gallery.js
@@ -9,15 +9,44 @@ window.onload = function() {
 
     var images = [];
 
-    for (var x = 0; x < 4; x++) {
+    var current = 0;
+
+    for (var x = 0; x < 12; x++) {
         images[x] = document.createElement('img');
         images[x].src = "images/gallery" + (x+1) + ".jpg";
         images[x].onclick = function() {
+            var currentNum = "";
+            console.log(this.src);
+
+            for (var y = 0; y < this.src.length; y++) {
+
+                var start = 0;
+
+                for (var a = 0; a < this.src.length; a++) {
+                    if (this.src[a] == "/") {
+                        start = a;
+                    }
+                }
+
+                for (var z = 0; z < "0123456789".length; z++) {
+                    if (this.src[y+start] == "0123456789"[z]) {
+                        currentNum += this.src[y+start];
+                    }
+                }
+            }
+            current = parseInt(currentNum)-1;
+            console.log(current);
             modal.style.display = 'block';
             modalImage.src = this.src;
+            if (this.height > this.width) {
+                modalImage.className = 'zoom-height';
+            } else {
+                modalImage.className = "zoom-width";
+            }
         };
         var imageDiv = document.createElement('div');
         imageDiv.className = "image-box";
+		imageDiv.style.backgroundImage = "url(images/gallery" + (x+1) + ".jpg)";
         imageDiv.appendChild(images[x]);
         container.appendChild(imageDiv);
     }
@@ -25,5 +54,26 @@ window.onload = function() {
     var close = document.getElementById('close');
     close.onclick = function() {
         modal.style.display = "none";
-    }
+		modalImage.className ="";
+        modalImage.style.maxHeight = '';
+    };
+
+    var leftarrow = document.getElementById('left-arrow');
+    var rightarrow = document.getElementById('right-arrow');
+
+    leftarrow.onclick = function() {
+        if (current > 0) {
+            current--;
+            modalImage.src = "images/gallery" + (current+1) + ".jpg";
+        }
+    };
+    rightarrow.onclick = function() {
+        console.log(images.length);
+        if (current < images.length - 1) {
+            current++;
+            modalImage.src = "images/gallery" + (current+1) + ".jpg";
+        }
+    };
+
+
 };
