3139c36649e22ff40b3c764b94ed800d0c5e5f44
ArtesOscuras <artesoscuras13@gmail.com>
2016-10-20 18:05:49 +0800

    2.69
    Another attempt at the form
    
    Form not yet fixed. Should be fixed by 3.0

--- form-handler.php
+++ form-handler.php
@@ -7,10 +7,6 @@
  */
 
 
-/** TODO: Eliminate 405
-    TODO: Add a form validator
- */
-
 $send_to = "artesoscuras13@gmail.com";
 
 $name = $_POST["name-input"];
@@ -19,8 +15,26 @@ $subject = $_POST["subject-input"];
 $message = $_POST["message"];
 
 $full_message = "Name: $name \n Email: $sender_email \n\n $message";
-
+/*
 mail($send_to, $subject, $full_message);
+*/
+$username = "PoliceTribuneAdmin@policetribune";
+$password = "1Sp4KP@ITH";
+
+
+try {
+/*    $conn = new PDO("jdbc:sqlserver://policetribune.database.windows.net:1433", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
+    echo "Done";
+/*    $new_id = $conn->prepare("SELECT MAX(id) FROM emails");
+    $new_id++;
+    $stmt = $conn->prepare("INSERT INTO emails VALUES ($new_id, $name, $sender_email, $subject, $message,  )");
+    mail($send_to, $subject, $full_message);
+*/
+} catch (Exception $e) {
+    echo "Failed.";
+}
+
 header("Location: thank_you.html");
 ?>
 <!doctype html>
