2d52dddd832c7a5c192b0eb2d4814cde1dfa7a36
ArtesOscuras <artesoscuras13@gmail.com>
2016-10-20 19:45:07 +0800

    2.697 - 1
                                              Another attempt at the form
    
                                              Form not yet fixed. Should be fixed by 3.0

---
+++ connect-test.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "policetribune.database.windows.net,1433";
+$database = "police_tribune";
+$username = "PoliceTribuneAdmin@policetribune";
+$password = "1Sp4KP@ITH";
+
+try {
+    $conn = new PDO("sqlsrv:server:=$server;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Succesful!";
+} catch (Exception $e) {
+    echo "Failed. " . $e->getMessage();
+}
